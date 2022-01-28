55363b40b5e390699f7806ed9061f9ff00b82e17
Ilya <webfox02@gmail.com>
2022-01-28 11:44:12 +0200

    init

--- ApiTest/ApiTest/ApiRequest.cs
+++ ApiTest/ApiTest/ApiRequest.cs
@@ -29,17 +29,20 @@ namespace TestProject1
 
             if (!headers.Any(h => h.Value.Contains("appllication/json")))
             {
-                foreach(var data in (Dictionary<string, string>)body){
-                    request.AddParameter(data.Key,data.Value);
+                foreach (var data in (Dictionary<string, string>)body)
+                {
+                    request.AddParameter(data.Key, data.Value);
                 }
             }
             else
             {
                 request.AddJsonBody(body);
+
                 request.RequestFormat = DataFormat.Json;
             }
 
             IRestResponse response = client.Execute(request);
+
             return response;
         }
 
@@ -59,7 +62,7 @@ namespace TestProject1
         public static List<Cookie> ExtractAllCookies(IRestResponse response)
         {
             List<Cookie> res = new List<Cookie>();
-            foreach(var cookie in response.Cookies)
+            foreach (var cookie in response.Cookies)
             {
                 res.Add(new Cookie(cookie.Name, cookie.Value, cookie.Domain, cookie.Path, null));
             }
@@ -69,13 +72,16 @@ namespace TestProject1
         public static void DownloadPicture(string url)
         {
             RestClient client = new RestClient(url);
+
             var pictureResponse = new RestRequest(Method.GET);
+
             byte[] result = client.DownloadData(pictureResponse);
-            File.WriteAllBytes(Path.Combine("/testPIC", "test1.jpg"), result); //downloads to D drive
+            //downloads to drive where's Studio installed
+            File.WriteAllBytes(Path.Combine("/testPIC", "test1.jpg"), result);
         }
 
-       
 
-        
+
+
     }
 }
